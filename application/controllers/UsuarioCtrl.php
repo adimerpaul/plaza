d12669d6d5001e56a24f13bdf21be681faeaead4
adimerpaul <adimer101@gmail.com>
2021-10-26 08:31:45 -0400

    modificado para un login

--- application/controllers/UsuarioCtrl.php
+++ application/controllers/UsuarioCtrl.php
@@ -6,7 +6,7 @@ class UsuarioCtrl extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('usuarios_model');
+		$this->load->model('Usuarios_model');
 		$this->load->model('temporal_model');
 
 
@@ -16,19 +16,19 @@ class UsuarioCtrl extends CI_Controller {
 		$this->session->sess_destroy();
 		$this->load->view('login');
 	}
-	
-	public function verifica() 
-    {	
-		
-		
+
+	public function verifica()
+    {
+
+
     	$usr=$this->input->post('user');
 		$con=$this->input->post('pass');
-	
-		$this->load->model('usuarios_model');
-		$respuesta=$this->usuarios_model->verificalogin($usr,$con);
 
-    	if ($respuesta) 
-    	{ 
+		$this->load->model('Usuarios_model');
+		$respuesta=$this->Usuarios_model->verificalogin($usr,$con);
+
+    	if ($respuesta)
+    	{
     		$datosusr = array(
     		'nombre' =>$respuesta[0]->nombreUser,
 			'cuenta' =>$respuesta[0]->user,
@@ -36,12 +36,12 @@ class UsuarioCtrl extends CI_Controller {
     		'idUs'=>$respuesta[0]->idUsuario,
     		'login'=>1
 		  );
-		  $this->usuarios_model->controlIngreso($respuesta[0]->idUsuario);
+		  $this->Usuarios_model->controlIngreso($respuesta[0]->idUsuario);
 		  $this->session->set_userdata($datosusr);
 
 		  $user = $this->session->userdata('idUs');
         	$this->temporal_model->deleteAll($user);
-		  	$dato=$this->usuarios_model->validaIngreso($user);
+		  	$dato=$this->Usuarios_model->validaIngreso($user);
 			  $this->load->view('templates/header',$dato);
 			  $this->load->view('inicio');
 			  $dato2['js']="<script>
@@ -50,7 +50,7 @@ class UsuarioCtrl extends CI_Controller {
 			  var dias=fechap.diff(fecha2,'days');
 			  //alert(dias);
 			  if(dias > 90) alert('Actualice su contraseña con el Administrador');
-			  </script>";    
+			  </script>";
 			  $this->load->view('templates/footer',$dato2);
 		 }
 		 else
@@ -62,18 +62,18 @@ class UsuarioCtrl extends CI_Controller {
 	    $this->session->sess_destroy();
 	    redirect('');
 	}
-	
+
 	public function usuarioreg()
 	{
 		if($this->session->userdata('login')==1){
-            
+
             $user = $this->session->userdata('idUs');
 
-            $dato=$this->usuarios_model->validaIngreso($user);
-           
+            $dato=$this->Usuarios_model->validaIngreso($user);
+
                 $this->load->view('templates/header', $dato);
 				$this->load->view('usuarioreg');
-                $dato2['js']="<script src='".base_url()."assets/js/usuario.js'></script>";    
+                $dato2['js']="<script src='".base_url()."assets/js/usuario.js'></script>";
                 $this->load->view('templates/footer',$dato2);
         }
         else redirect('');
@@ -83,15 +83,15 @@ class UsuarioCtrl extends CI_Controller {
 	public function usuariover()
 	{
 		if($this->session->userdata('login')==1){
-            
+
             $user = $this->session->userdata('idUs');
 
-            $dato=$this->usuarios_model->validaIngreso($user);
-           
-            $usuario['usuario'] = $this->usuarios_model->listarUsuario();
+            $dato=$this->Usuarios_model->validaIngreso($user);
+
+            $usuario['usuario'] = $this->Usuarios_model->listarUsuario();
 			$this->load->view('templates/header', $dato);
 				$this->load->view('usuariover',$usuario);
-                $dato2['js']="<script src='".base_url()."assets/js/usuario.js'></script>";    
+                $dato2['js']="<script src='".base_url()."assets/js/usuario.js'></script>";
                 $this->load->view('templates/footer',$dato2);
         }
         else redirect('');
@@ -101,14 +101,14 @@ class UsuarioCtrl extends CI_Controller {
         $user=$_POST['user1'];
 		$query=$this->db->query("SELECT * FROM usuario WHERE user='$user'");
 		if($query->num_rows() > 0 ){
-        $row=$query->row();        
+        $row=$query->row();
         $myObj=($query->result_array())[0];
 		echo json_encode($myObj);}
 		else
 		echo '{"user":""}';
-		
+
 	}
-	
+
     public function datoUser(){
         $idusuario=$_POST['idusuario'];
         $query=$this->db->query("SELECT * FROM usuario WHERE idUsuario='$idusuario'");
@@ -120,7 +120,7 @@ class UsuarioCtrl extends CI_Controller {
 
 	public function userpermiso(){
         $idusuario=$_POST['idusuario'];
-		$query=$this->db->query("SELECT * FROM permiso WHERE idUsuario='$idusuario'");		        
+		$query=$this->db->query("SELECT * FROM permiso WHERE idUsuario='$idusuario'");
         $myObj=($query->result_array());
 
         echo json_encode($myObj);
@@ -129,24 +129,24 @@ class UsuarioCtrl extends CI_Controller {
 	public function recuperaSeccion(){
         $query=$this->db->query("SELECT * FROM seccion");
         $row=$query->row();
-        
+
         $myObj=($query->result_array());
 
         echo json_encode($myObj);
 	}
-	
-	
+
+
 	public function store(Type $var = null)
 	{
-	    $idU=$this->usuarios_model->store();
+	    $idU=$this->Usuarios_model->store();
 
 		$query=$this->db->query("SELECT * FROM seccion");
 		foreach($query->result() as $row){
 			if(isset($_POST['s'.$row->idSeccion])){
-				
-				$this->usuarios_model->regpermiso($idU,$row->idSeccion);
+
+				$this->Usuarios_model->regpermiso($idU,$row->idSeccion);
 			}
-			
+
 		}
 
 		$this->usuarioreg();
@@ -154,39 +154,39 @@ class UsuarioCtrl extends CI_Controller {
 
 	public function update()
 	{
-		$this->usuarios_model->updateUS();
-		$idU=$this->input->post('idusuario1');	
+		$this->Usuarios_model->updateUS();
+		$idU=$this->input->post('idusuario1');
 		$this->eliminarPermiso($idU);
 		$query=$this->db->query("SELECT * FROM seccion");
-	
+
 		foreach($query->result() as $row){
 			if(isset($_POST['s'.$row->idSeccion])){
-				$this->usuarios_model->regpermiso($idU,$row->idSeccion);
-				echo $row->idSeccion;				}					
+				$this->Usuarios_model->regpermiso($idU,$row->idSeccion);
+				echo $row->idSeccion;				}
 		}
 
 		header("Location: ".base_url()."UsuarioCtrl/usuariover");
 	}
 
 	public function updatepass(){
-		$this->usuarios_model->updatepassword();
+		$this->Usuarios_model->updatepassword();
         header("Location: ".base_url()."UsuarioCtrl/usuariover");
-			
+
 	}
 
 	public function eliminarPermiso($idU){
 		$this->db->where('idUsuario',$idU);
 		return $this->db->delete('permiso');
 	}
-	
+
 	public function validapass($idU){
 		$this->db->where('idUsuario',$idU);
 		$resultado=$this->db->get('usuario');
 		return $resultado->result();
-	} 
+	}
 
 	public function delete($id){
-		$this->usuarios_model->delete($id);
+		$this->Usuarios_model->delete($id);
         header("Location: ".base_url()."UsuarioCtrl/usuariover");
 	}
 
@@ -195,7 +195,7 @@ class UsuarioCtrl extends CI_Controller {
 			'format'        => 'zip',                       // gzip, zip, txt
 			'filename'      => 'backupmulticine.sql',              // File name - NEEDED ONLY WITH ZIP FILES
 				);
-	
+
 	//$this->dbutil->backup($prefs);
 	// Load the DB utility class
 	$this->load->dbutil();
